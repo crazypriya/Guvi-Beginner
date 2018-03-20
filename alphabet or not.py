87689b7ae7a566f2aea5b06271991d9af5875302
15EC049 <umarbasheer1996@gmail.com>
2018-03-20 16:06:17 +0530

    Update alphabet or not.py

--- alphabet or not.py
+++ alphabet or not.py
@@ -1,5 +1,5 @@
-val=str(input())
-    if(((val>='a')and(val<='z'))or(val>='A')and(val<='Z')):
-        print("alphabet")
-    else:
-        print("not an alphabet")
+ val=str(input())
+     if(((val>='a')and(val<='z'))or(val>='A')and(val<='Z')):
+         print("alphabet")
+     else:
+         print("not an alphabet")
